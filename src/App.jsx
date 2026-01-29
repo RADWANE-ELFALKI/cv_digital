4a156eb9b9085495ca7650b5b8da6b7bfd9d7133
userredwane <radwanelfalki@gmail.com>
2026-01-29 20:22:31 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+
+import { Navbar, Welcome, Dock, Home } from "#components";
+import {Finder, Resume, Safari, Terminal, Text, Image, Contact, Photos} from "#windows";
+
+gsap.registerPlugin(Draggable);
+
+const App = () => {
+    return (
+        <main>
+            <Navbar />
+            <Welcome />
+            <Dock />
+
+            <Terminal />
+            <Safari />
+            <Resume />
+            <Finder />
+            <Text />
+            <Image />
+            <Contact />
+            <Home />
+            <Photos />
+        </main>
+    )
+}
+export default App;
